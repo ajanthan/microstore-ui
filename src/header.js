2a6d99cd5a1a5db66d61ced92389e0a1dd28c2e1
Ajanthan Balachandran <balaajanthan@gmail.com>
2020-05-01 18:47:14 -0700

    adding login

--- src/header.js
+++ src/header.js
@@ -1,26 +1,28 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { UserContext } from '@ajanthan/wso2identity-js';
 
-class Header extends React.Component {
-    render() {
-        return (
-            <nav className="navbar navbar-light bg-primary justify-content-between" >
-                <a className="navbar-brand">ACM Store</a>
-                <div className="nav-item dropdown">
-                    <div className="nav-item dropdown-toggle" href="#"
-                        id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                    </div>
-                    <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink" >
-                        <form className="form" method="post">
-                            <button className="btn btn-primary btn-link" type="submit" value="Sign Out">Sign Out</button>
-                        </form>
-                        <a className="menu-item" href="/cart">checkout</a>
-                    </div>
-                </div>
-            </nav>
-        );
+const Header = () => {
+    const userContext = useContext(UserContext);
+    const [toggle, setToggle] = useState(false);
+    const handleToggle = () => {
+        setToggle(!toggle);
     }
+    return (
+        <nav className="navbar navbar-light bg-primary justify-content-between" >
+            <a className="navbar-brand">ACM Store</a>
+            <div className=" nav-item dropdown">
+                <button class="btn btn-primary dropdown-toggle" type="button"
+                    data-toggle="dropdown" onClick={handleToggle}>More
+                </button>
+                    <ul className="dropdown-menu dropdown-menu-right" style={{display: toggle? "block":"none"}}>
+                        <li>  <a className="dropdown-item" onClick={userContext.handleLogout}>Sign Out</a></li>
+                        <li>  <a className="dropdown-item" href="/cart">checkout</a></li>
+                    </ul>
+            </div>
+        </nav>
+    );
 }
 export default Header;
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,21 @@
 import ReactDom from 'react-dom';
-import React from 'react'
-import App from './app'
+import React from 'react';
+import App from './app';
+import {UserContextProvider} from '@ajanthan/wso2identity-js';
 
-ReactDom.render(<App />,
+const loginConfig = {
+    client_id: 'A3x7hnhf3zCWKFfrs50fnof53xAa',
+    redirect_url: 'https://ajanthan.github.io/microstore-ui/redirect.html',
+    logout_url: 'https://identity.us.to/oidc/logout',
+    epConfig: {
+      authorization_endpoint: 'https://identity.us.to/oauth2/authorize',
+      token_endpoint: 'https://identity.us.to/oauth2/token',
+      revocation_endpoint: 'https://identity.us.to/oauth2/revoke',
+      userinfo_endpoint: 'https://identity.us.to/oauth2/userinfo'
+    }
+  };
+
+ReactDom.render(
+    <UserContextProvider config={loginConfig}><App /></UserContextProvider>
+,
     document.getElementById('root'));
